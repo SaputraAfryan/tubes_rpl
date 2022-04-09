277eba527b8caba06366aae0ff35d8bd8cfdc0e6
SaputraAfryan <afryan54@gmail.com>
2022-04-09 20:59:56 +0700

    Tubes RPL SkutKost!

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+@include('layouts.main')
+
+@section('container')
+    <div class="container-category" style="padding:20px;background-color: #00AFC1">
+        <div class="container-category-header">
+            <span style="color: #FAFDD6">
+                Kategori
+            </span>
+        </div>
+        <div class="container-category-content" style="padding: 20px;background-color:#00AFC1">
+
+        </div>
+    </div>
+
+@show
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-light" style="background-color: #afeeee">
+    <div class="container">
+      <a class="navbar-brand" href="/">
+        <img src="logo.png" alt="SkutKost!-logo.png" width="95" height="95" class="d-inline-block align-text-middle rounded-circle">
+        SkutKost!
+      </a>
+      <div class="d-flex flex-row-reverse">
+        <div class="p-2">
+          <a href="/" class="btn btn-secondary" role="button">Daftar</a>
+        </div>
+        <div class="p-2">
+          <a href="/" class="btn btn-primary" role="button">Login</a>
+        </div>
+        <div class="p-2">
+            <form class="d-flex">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>   
+        </div>
+      </div>
+    </div>
+  </nav>
