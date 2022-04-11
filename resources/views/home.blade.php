32adff544c887bfef8b3868011d50753130b8404
SaputraAfryan <afryan54@gmail.com>
2022-04-11 20:08:30 +0700

    update main.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,19 +1,38 @@
 @include('layouts.main')
 
 @section('container')
-    <div class="container-category fluid" style="padding:20px;background-color: #00AFC1">
+    <div class="container-category fluid" style="padding:20px;background-color: #EED7CE">
         <div class="container-category-header">
-            <h2>
-                <span style="color: #FAFDD6">
-                    Kategori
-                </span>
-            </h2>
+            <table style="width: 100%">
+                <tr>
+                    <td align="left">
+                        <div style="color: #F7F7F7">
+                            <h2>Kategori</h2>
+                        </div>
+                    </td>
+                    <td align="right"><a href="/" type="link">See More</a></td>
+                </tr>
+            </table>
         </div>
-        <div class="container-category-content" style="padding: 20px;background-color:#00AFC1">
-            <p>
-                astagfirullah
-            </p>
+        <div class="container-category-content" style="padding: 20px;background-color:#EED7CE">
+            <table style="width:50%">
+                <tr>
+                    <td align="right">
+                        row 1 col 1
+                    </td>
+                    <td align="left">
+                        row 1 col 2
+                    </td>
+                </tr>
+                <tr>
+                    <td align="right">
+                        row 2 col 1
+                    </td>
+                    <td align="left">
+                        row 2 col 2
+                    </td>
+                </tr>
+            </table>
         </div>
     </div>
-
 @show
