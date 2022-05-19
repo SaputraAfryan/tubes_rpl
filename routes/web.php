748718ea8341bbe078ff46902d8140d3b989ad12
SaputraAfryan <afryan54@gmail.com>
2022-05-19 15:32:57 +0700

    added user role

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,13 +15,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
-});
- 
-Route::get('/signin', function(){
-    return view('signin');
+    return view('welcome');
 });
 
-Route::get('/signup', function () {
-    return view('signup');
+//auth route for both
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 });
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
